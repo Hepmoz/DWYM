baa94d1de92d0f6506bb58ebb3466f703c2e6b3c
Héctor Pérez <hector.perez.m@protonmail.com>
2018-09-30 20:30:11 -0300

    Added basic ui scheme following a tutorial

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { UiModule } from './ui/ui.module';
+import { ExpenseComponent } from './expense/expense.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpenseComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    UiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
